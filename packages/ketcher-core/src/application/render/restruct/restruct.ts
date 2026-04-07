eeecd600e95342bc8b7a3a9c9382b31a6516dab4
mariam-khutuashvili <mariam_khutuashvili@epam.com>
2026-04-07 13:06:24 +0400

    #9625 – Convert reloop.js to TypeScript with prop type definitions

--- packages/ketcher-core/src/application/render/restruct/restruct.ts
+++ packages/ketcher-core/src/application/render/restruct/restruct.ts
@@ -626,7 +626,7 @@ class ReStruct {
       this.markBond(bid, 1);
     });
     ret.newLoops.forEach((loopId) => {
-      this.reloops.set(loopId, new ReLoop(this.molecule.loops.get(loopId)));
+      this.reloops.set(loopId, new ReLoop(this.molecule.loops.get(loopId)!));
     });
   }
 
